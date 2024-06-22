448a680930b75c9c09dcb153e60a36fa52232163
vitor-gm <gm.0509rbt@gmail.com>
2024-06-22 14:01:15 -0300

    Implementação da UML

---
+++ src/Iphone.java
@@ -0,0 +1,46 @@
+public class Iphone implements AparelhoTelefonico, ReprodutorMusical, NavegadorNaInternet{
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+}
---
+++ src/NavegadorNaInternet.java
@@ -0,0 +1,7 @@
+public interface NavegadorNaInternet {
+
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+
+}
